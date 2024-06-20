ce9350bbc7f4f3f0ffcf8c9d8ac3ff9c8443492f
Jsejejdh <147077171+Jsejejdh@users.noreply.github.com>
2024-06-20 12:02:52 +0530

    BMI.py

---
+++ BMI.py
@@ -0,0 +1,14 @@
+# calculator
+a = float(input("enter the first number"))
+b = float(input("enter the first number"))
+
+print("1. ADD , 2. SUB , 3. MUL , 4. DIV")
+s = int(input("enter the number based on the operations you want to perform"))
+if s == 1:
+    print(a + b)
+if s == 2:
+    print(a - b)
+if s == 3:
+    print(a * b)
+if s == 4:
+    print( a / b)
